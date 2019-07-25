ad9bacec5ab6a4c6906c739ad18e5b9e4c331720
Nils Hartmann <nils@nilshartmann.net>
2019-07-25 13:06:34 +0200

    app-thunk from react-training example

--- app-thunk/src/App.js
+++ app-thunk/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GreetingController from "./GreetingController";
+import Counter from "./Counter";
+import Chart from "./Chart";
 
-function App() {
+import { useSelector, useDispatch } from "react-redux";
+import LoadingIndicator from "./LoadingIndicator";
+
+import { loadGreetingsFromServer } from "./greeting-actions";
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isRequestRunning = useSelector(state => state.greetings.requestRunning);
+  const requestDescription = useSelector(state => state.greetings.requestDescription);
+  const loadingError = useSelector(state => state.greetings.error);
+
+  React.useEffect(() => {
+    dispatch(loadGreetingsFromServer());
+  }, [dispatch]);
+
+  if (loadingError) {
+    return (
+      <div className="Main">
+        <div className="Title">
+          <h1>Greeting App</h1>
+        </div>
+        <h1>Error while loading data!</h1>
+        <p>{loadingError.toString()}</p>
+        <button onClick={() => dispatch(loadGreetingsFromServer())}>Try again</button>
+      </div>
+    );
+  }
+
+  if (isRequestRunning) {
+    return (
+      <div className="Main">
+        <div className="Title">
+          <h1>Greeting App</h1>
+        </div>
+        <LoadingIndicator label={requestDescription} />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+      <div className="Title">
+        <h1>Greeting App</h1>
+        <Counter />
+      </div>
+      <div className="Left">
+        <GreetingController />
+      </div>
+      <div className="Right">
+        <Chart />
+      </div>
     </div>
   );
 }
-
-export default App;
